6c5f97c37dc137d9622a6f550bad16d16bcee6fe
dera- <alchemy.and.fairy@gmail.com>
2019-06-19 15:06:12 +0900

    reflect comment

--- build/republishAndUpdateChangelog.js
+++ build/republishAndUpdateChangelog.js
@@ -6,15 +6,6 @@ const apiBaseUrl = "https://api.github.com/repos/akashic-games/headless-driver";
 const pullRequestBody = "※自動作成されたPRです";
 const pullRequestLabel = "republish";
 
-function execCommand(command) {
-	const result = execSync(command);
-	if (execSync("echo $?").toString().replace("\n", "") !== "0") {
-		console.error(`Failed: ${command}.`);
-		process.exit(1);
-	}
-	return result;
-}
-
 if (process.argv.length < 3) {
 	console.error("Please enter command as follows: node republishAndUpdateChangelog.js [patch|minor|major]");
 	process.exit(1);
@@ -36,18 +27,18 @@ if (process.env.GITHUB_AUTH == null) {
 
 // versionのbump処理
 console.log("start to bump version");
-const branchName = Date.now();
+const branchName = "tmp_" + Date.now();
 const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
 // versionのbumpを行う前の準備作業
-execCommand("git fetch");
-execCommand("git checkout origin/master");
-execCommand(`git checkout -b ${branchName}`);
+execSync("git fetch");
+execSync("git checkout origin/master");
+execSync(`git checkout -b ${branchName}`);
 // 直前のcommitがlernaでversionをbumpしたcommitの場合、versionのbumpに失敗するので空コミットを一度挟んでおく
-execCommand("git commit --allow-empty -m 'empty'");
+execSync("git commit --allow-empty -m 'empty'");
 // versionをbumpするためにはリモート側にブランチを用意しておく必要がある
-execCommand(`git push origin ${branchName}`);
+execSync(`git push origin ${branchName}`);
 // versionのbumpしてcommit+push(ここでgithubリポジトリにタグとリリースノートが作成される)
-execCommand(`${lernaPath} version ${target} --allow-branch=* --force-publish=* --yes`);
+execSync(`${lernaPath} version ${target} --allow-branch=${branchName} --force-publish=* --yes`);
 console.log("end to bump version");
 
 // PRの作成とマージ処理
@@ -62,21 +53,21 @@ execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X PO
 // PRのマージ
 execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X PUT ${apiBaseUrl}/pulls/${pullReqData["number"]}/merge`);
 // ブランチ削除
-execCommand("git checkout origin/master");
-execCommand(`git branch -D ${branchName}`);
-execCommand(`git push origin :${branchName}`);
+execSync("git checkout origin/master");
+execSync(`git branch -D ${branchName}`);
+execSync(`git push origin :${branchName}`);
 console.log("end to merge PR");
 
 // 現在のCHANGELOGに次バージョンのログを追加
 console.log("start to update changelog");
-execCommand("git checkout master");
-execCommand("git pull origin master");
+execSync("git checkout master");
+execSync("git pull origin master");
 const lernaChangeLogPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog");
-const addedLog = execCommand(`${lernaChangeLogPath} --next-version v${currentVersion}`).toString();
+const addedLog = execSync(`${lernaChangeLogPath} --next-version v${currentVersion}`).toString();
 const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
 const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
 fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
-execCommand("git add ./CHANGELOG.md");
-execCommand("git commit -m 'Update Changelog'");
-execCommand("git push origin master");
+execSync("git add ./CHANGELOG.md");
+execSync("git commit -m 'Update Changelog'");
+execSync("git push origin master");
 console.log("end to update changelog");
