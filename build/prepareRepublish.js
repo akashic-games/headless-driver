a0a39d68407a77f34a51a89bd4bec3a5eb141bb7
dera- <alchemy.and.fairy@gmail.com>
2019-06-24 21:34:25 +0900

    reflect comment

--- build/prepareRepublish.js
+++ build/prepareRepublish.js
@@ -1,26 +1,11 @@
-const path = require("path");
 const execSync = require("child_process").execSync;
-const updateChangelog = require("./parts/updateChangelog");
 
 const apiBaseUrl = "https://api.github.com/repos/akashic-games/headless-driver";
 const pullRequestTitle = "Republish";
 const pullRequestBody = "再publishを行うためにラベルのみ付与したPullRequestです。何も変更はありません。";
 const pullRequestLabel = "republish";
 
-if (process.argv.length < 3) {
-	console.error("Please enter command as follows: node republishAndUpdateChangelog.js [patch|minor|major]");
-	process.exit(1);
-}
-
-// どのバージョンを上げるのかを取得
-const target = process.argv[2];
-if (! /^patch|minor|major$/.test(target)) {
-	console.error("Please specify patch, minor or major.");
-	process.exit(1);
-}
-
-// lerna-changelogコマンドを実行するために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要がある。
-// しかし、与えられていなくてもコマンド実行時にエラーは発生しないのでここで事前にチェックする。
+// PRの作成・マージを行うために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要がある。
 if (process.env.GITHUB_AUTH == null) {
 	console.error("Must provide GITHUB_AUTH.");
 	process.exit(1);
@@ -48,29 +33,12 @@ try {
 	execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X POST -d '{"labels": ["${pullRequestLabel}"]}' ${apiBaseUrl}/issues/${pullReqData["number"]}/labels`);
 	// PRのマージ
 	execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X PUT ${apiBaseUrl}/pulls/${pullReqData["number"]}/merge`);
-	// ブランチ削除
-	execSync("git checkout origin/master");
+	// ブランチ削除とmasterの更新
+	execSync("git checkout master");
 	execSync(`git branch -D ${branchName}`);
 	execSync(`git push origin :${branchName}`);
-	console.log("end to merge PR");
-
-	// publish処理
-	console.log("start to publish");
-	execSync("git checkout master");
 	execSync("git pull origin master");
-	// CHANGELOG作成時に必要になるのでpublish前のバージョンを保持しておく
-	const beforeVersion = require(path.join(__dirname, "..", "lerna.json")).version;
-	execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna")} publish ${target} --force-publish=* --yes`);
-	console.log("end to publish");
-
-	// 現在のCHANGELOGに次バージョンのログを追加
-	// 新しくタグを打ってからでないと前回のタグからの更新内容が取得できないため、CHANGELOGへの書き込みはpublish後に行う
-	console.log("start to update changelog");
-	updateChangelog(beforeVersion);
-	execSync("git add ./CHANGELOG.md");
-	execSync("git commit -m 'Update Changelog'");
-	execSync("git push origin master");
-	console.log("end to update changelog");
+	console.log("end to merge PR");
 } catch (e) {
 	console.error(e);
 	process.exit(1);
--- build/publishAndUpdateChangelog.js
+++ build/publishAndUpdateChangelog.js
@@ -1,6 +1,6 @@
 const path = require("path");
+const fs = require("fs");
 const execSync = require("child_process").execSync;
-const updateChangelog = require("./parts/updateChangelog");
 
 if (process.argv.length < 3) {
 	console.error("Please enter command as follows: node publishAndUpdateChangelog.js [patch|minor|major]");
@@ -11,13 +11,6 @@ if (process.argv.length < 3) {
 const target = process.argv[2];
 if (! /^patch|minor|major$/.test(target)) {
 	console.error("Please specify patch, minor or major.");
-	process.exit(1);
-}
-
-const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
-// 更新するモジュールが無ければChangelog更新処理を行わず終了する
-if (parseInt(execSync(`${lernaPath} changed | wc -l`).toString(), 10) === 0) {
-	console.error("No modules to update version.");
 	process.exit(1);
 }
 
@@ -33,13 +26,18 @@ try {
 	console.log("start to publish");
 	// CHANGELOG作成時に必要になるのでpublish前のバージョンを保持しておく
 	const beforeVersion = require(path.join(__dirname, "..", "lerna.json")).version;
+	const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
 	execSync(`${lernaPath} publish ${target} --yes`);
 	console.log("end to publish");
 
 	// 現在のCHANGELOGに次バージョンのログを追加
 	// 新しくタグを打ってからでないと前回のタグからの更新内容が取得できないため、CHANGELOGへの書き込みはpublish後に行う
 	console.log("start to update changelog");
-	updateChangelog(beforeVersion);
+	const lernaChangeLogPath = path.join(__dirname, "..", "..", "node_modules", ".bin", "lerna-changelog");
+	const addedLog = execSync(`${lernaChangeLogPath} --from v${beforeVersion}`).toString();
+	const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "..", "CHANGELOG.md")).toString();
+	const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
+	fs.writeFileSync(path.join(__dirname, "..", "..", "CHANGELOG.md"), nextChangeLog);
 	execSync("git add ./CHANGELOG.md");
 	execSync("git commit -m 'Update Changelog'");
 	execSync("git push origin master");
