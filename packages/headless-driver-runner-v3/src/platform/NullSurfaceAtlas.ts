71f73f4c164ad9623d7f12dd5010b4f8bd32e31e
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2020-07-30 19:15:24 +0900

    Follow engine-files 3.0.0-beta7

--- packages/headless-driver-runner-v3/src/platform/NullSurfaceAtlas.ts
+++ packages/headless-driver-runner-v3/src/platform/NullSurfaceAtlas.ts
@@ -1,32 +1,11 @@
 import { akashicEngine as g } from "@akashic/engine-files";
-import { SurfaceAtlasSlot } from "@akashic/pdi-common-impl";
 
-export class NullSurfaceAtlas implements g.SurfaceAtlas {
-	_surface: g.Surface;
-	_accessScore: number;
-	_emptySurfaceAtlasSlotHead: SurfaceAtlasSlot;
-	_usedRectangleAreaSize: g.CommonSize;
-
+export class NullSurfaceAtlas extends g.SurfaceAtlas {
 	constructor(surface: g.Surface) {
-		this._surface = surface;
-		this._accessScore = 0;
-		this._emptySurfaceAtlasSlotHead = new SurfaceAtlasSlot(0, 0, this._surface.width, this._surface.height);
-		this._usedRectangleAreaSize = { width: 0, height: 0 };
+		super(surface);
 	}
 
-	addSurface(_surface: g.Surface, _offsetX: number, _offsetY: number, _width: number, _height: number): SurfaceAtlasSlot {
+	addSurface(_surface: g.Surface, _offsetX: number, _offsetY: number, _width: number, _height: number): g.SurfaceAtlasSlot {
 		return this._emptySurfaceAtlasSlotHead;
-	}
-
-	getAtlasUsedSize(): g.CommonSize {
-		return this._usedRectangleAreaSize;
-	}
-
-	destroy(): void {
-		this._surface.destroy();
-	}
-
-	destroyed(): boolean {
-		return this._surface.destroyed();
 	}
 }
